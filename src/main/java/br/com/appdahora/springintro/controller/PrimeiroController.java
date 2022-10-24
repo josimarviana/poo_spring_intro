556ebc29a5232fb8ab85142c033e4fd900fdcc72
Josimar Viana Silva <josimarviana@gmail.com>
2022-10-24 11:40:27 -0300

    sem usar spring

--- src/main/java/br/com/appdahora/springintro/controller/PrimeiroController.java
+++ src/main/java/br/com/appdahora/springintro/controller/PrimeiroController.java
@@ -2,6 +2,7 @@ package br.com.appdahora.springintro.controller;
 
 import br.com.appdahora.springintro.models.Carro;
 import br.com.appdahora.springintro.models.MotorEsportivo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
--- src/main/java/br/com/appdahora/springintro/models/Carro.java
+++ src/main/java/br/com/appdahora/springintro/models/Carro.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class Carro {
+
     private MotorEsportivo motor;
     private String modelo;
 
